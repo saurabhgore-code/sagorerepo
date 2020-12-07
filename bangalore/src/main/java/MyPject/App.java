60e5bf0b45fd4ea27f950906a9ae9dc270d2159d
root <root@ip-172-31-88-62.ec2.internal>
2020-12-07 09:01:10 +0000

    modified one file

---
+++ bangalore/src/main/java/MyPject/App.java
@@ -0,0 +1,13 @@
+package MyPject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
